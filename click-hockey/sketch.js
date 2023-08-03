840a8f16941c760294d1d6eb003f2fa3092afd1d
BryanASC23 <139984244+BryanASC23@users.noreply.github.com>
2023-08-03 14:01:48 -0400

    Update sketch.js

--- click-hockey/sketch.js
+++ click-hockey/sketch.js
@@ -16,26 +16,27 @@ function draw() {
     background(222);
 
     // draw goal
-    fill(0, 255 0);
+    fill(0, 255, 0);
     rect(250, 490, 60, 20);
 
     // draw ball
     fill(255, 0, 255);
-    rect(30, xPos, yPos);
+    rect(xPos, yPos, 30, 30);
 
     // draw score
     fill(18);
     textSize(20);
-    text("Score: " + score, 0, 0)
+    text("Score: " + score, 30, 30)
 
     // check if in goal
-    if (xPos + 15 <= 220 && xPos - 15 <= 280 && yPos + 15 >= 480) {
+    if (xPos + 15 >= 220 && xPos - 15 <= 280 && yPos + 15 >= 480) {
         score;
         // reset position and speed
         xPos = random(15, 485);
         yPos = 0;
         xSpeed = random(-5, 5);
         ySpeed = random(0, 10);
+        score ++;
     }
 
     // move forward
@@ -51,11 +52,12 @@ function draw() {
     }
     if (yPos > 500) {
         yPos = 0;
-        score--;
+        //score--;
     }
 }
 
 function mouseClicked() {
-    xSpeed = random(-5, 5);
-    ySpeed = random(0, 5);
+    xSpeed = random(1, 3);
+    ySpeed = random(1, 3);
+    xSpeed * -1;
 }
